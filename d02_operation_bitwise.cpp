6e38ab5977b61312205972babd07263b6a90b764
Nhân Lương <nhanluong@Nhans-MacBook-Air.local>
2021-11-28 11:02:30 +0700

    Add csrf protection

---
+++ d02_operation_bitwise.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*demo phep toan nhi phan*/
+int main() {
+    int a = 10, b = 15;
+    printf(">> Phep toan luan ly nhi phan (bitwise logical): ");
+    printf(" bitwise and : %d & %d = %d \n", a, b, a&b);
+    printf("bitwise or   : %d | %d = %d \n", a, b, a|b);
+    printf("bitwise xor  : %d ^ %d = %d \n, a, b, a^b");
+    printf("bitwise not  : ~%d = %d \n", a, ~a);
+}
